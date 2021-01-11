a665c0b887dfa59b5c6c131c3e7fbe15841f0827
defalt18 <45987083+defalt18@users.noreply.github.com>
2021-01-11 14:12:28 +0530

    Removed credentials

--- eventman/bin/eventman_defalt/eventman_defalt/settings.py
+++ eventman/bin/eventman_defalt/eventman_defalt/settings.py
@@ -129,5 +129,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'itschloe317@gmail.com'
-EMAIL_HOST_PASSWORD = 'hwing317'
+EMAIL_HOST_USER = 'XXXXXXXXXX'
+EMAIL_HOST_PASSWORD = 'XXXXXXX'
